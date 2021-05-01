2700f5ff4bd532c090f6c9f4ac0aca58147f0fb9
Bob Loblaw <bobloblaw@selfdocumentingcode>
2021-05-01 11:02:00 +0200

    Add support for big unicode emojis (fetch external image url)

--- Kattbot/CommandModules/RandomModule.cs
+++ Kattbot/CommandModules/RandomModule.cs
@@ -3,6 +3,7 @@ using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
 using Kattbot.Attributes;
 using System;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Kattbot.CommandModules
@@ -35,9 +36,56 @@ namespace Kattbot.CommandModules
         [Command("big")]
         public Task BigEmote(CommandContext ctx, DiscordEmoji emoji)
         {
-            return ctx.RespondAsync(emoji.Url);
+            string url;
+
+            if (emoji.Id != 0)
+            {
+                url = emoji.Url;
+            }
+            else
+            {
+                url = GetExternalEmojiImageUrl(emoji.Name);
+            }
+
+            return ctx.RespondAsync(url);
+        }
+
+        private string GetExternalEmojiImageUrl(string code)
+        {
+            // eggplant = 0001F346
+            // https://emoji.aranja.com/static/emoji-data/img-twitter-72/1f346.png
+
+            // flag =  0001F1E6 0001F1E9
+            // https://emoji.aranja.com/static/emoji-data/img-twitter-72/1f1e6-1f1e9.png
+
+            var utf32Encoding = new UTF32Encoding(true, false);
+
+            var bytes = utf32Encoding.GetBytes(code);
+
+            var sb = new StringBuilder();
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                sb.AppendFormat("{0:X2}", bytes[i]);
+            }
+
+            var bytesAsString = sb.ToString();
+
+            var fileNameBuilder = new StringBuilder();
+
+            for (int i = 0; i < bytesAsString.Length; i += 8)
+            {
+                var unicodePart = bytesAsString.Substring(i, 8)
+                    .TrimStart('0')
+                    .ToLower();
+
+                fileNameBuilder.Append(i == 0 ? unicodePart : $"-{unicodePart}");
+            }
+
+            var fileName = fileNameBuilder.ToString();
+
+            return $"https://emoji.aranja.com/static/emoji-data/img-twitter-72/{fileName}.png";
         }
     }
 
-    
+
 }
