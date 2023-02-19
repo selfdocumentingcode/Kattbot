714cf152bb7ebef22afa68f93a4ee14beb1adbaf
Bob Loblaw <razvanmandache@gmail.com>
2023-02-19 18:39:57 +0100

    OK NOW ITS ON

--- deploy/kattbot-deploy.sh
+++ deploy/kattbot-deploy.sh
@@ -2,12 +2,11 @@
 
 # remove directory
 rm -rf $HOME/kattbot
-
-# recreate it
 mkdir -p $HOME/kattbot
 
 # copy files from staging to app directory
 cp -r $HOME/kattbot-staging/. $HOME/kattbot
 
 # copy service file and create path if it doesn't exist
-cp -p $HOME/kattbot-deploy/kattbot.service $HOME/.config/systemd/user
+mkdir -p $HOME/.config/systemd/user
+cp $HOME/kattbot-deploy/kattbot.service $HOME/.config/systemd/user/
