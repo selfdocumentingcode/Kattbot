d2edf0e5ee6b247b22774da93da933c5ffa393b1
Bob Loblaw <iwrite@selfdocumentingco.de>
2024-07-22 21:27:17 +0200

    Install headless chrome deps in container image

--- Kattbot/Services/PuppeteerFactory.cs
+++ Kattbot/Services/PuppeteerFactory.cs
@@ -1,7 +1,4 @@
-﻿using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using PuppeteerSharp;
 
 namespace Kattbot.Services;
@@ -10,27 +7,17 @@ public class PuppeteerFactory
 {
     public async Task<IBrowser> BuildBrowser()
     {
-        string downloadPath = Path.Combine(Path.GetTempPath(), "puppeteer-kattbot");
+        var browserFetcher = new BrowserFetcher();
 
-        var browserFetcherOptions = new BrowserFetcherOptions { Path = downloadPath };
-        var browserFetcher = new BrowserFetcher(browserFetcherOptions);
-        await browserFetcher.DownloadAsync(BrowserFetcher.DefaultChromiumRevision);
+        _ = await browserFetcher.DownloadAsync();
 
-        string executableDirectory = Directory.EnumerateDirectories(
-                Directory.EnumerateDirectories(downloadPath)
-                    .First())
-            .First();
-
-        string executableFilename = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
-            ? "chrome.exe"
-            : "chrome";
-
-        string executablePath = Path.Combine(executableDirectory, executableFilename);
-
-        var options = new LaunchOptions { Headless = true, ExecutablePath = executablePath };
+        var options = new LaunchOptions
+        {
+            Headless = true,
+            Args = ["--no-sandbox"],
+        };
 
         IBrowser browser = await Puppeteer.LaunchAsync(options);
-
         return browser;
     }
 }
